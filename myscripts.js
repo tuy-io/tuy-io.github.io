1dc2d42049063d40884b3f92150938d16f32c2a7
stuy <stuy@newroads.org>
2017-01-25 23:22:04 -0500

    add app

--- myscripts.js
+++ myscripts.js
@@ -10,7 +10,7 @@ function getNumbers(){
 }
 
 function checkAnswer(){
-  var userSum = document.getElemendById("userAnswer").value;
+  var userSum = document.getElementById("userAnswer").value;
   var sum = operand1 + operand2;
   if(userSum == sum){
     document.getElementById("results").innerHTML="Correct!";
