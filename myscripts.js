e945e971d2cbcf49b7dc42aeb1fdcf40e4883d51
stuy <stuy@newroads.org>
2017-01-28 13:12:47 -0500

    add html for flashcard

--- myscripts.js
+++ myscripts.js
@@ -5,5 +5,7 @@ var operand2;
 function getNumbers(){
   operand1 = Math.floor(Math.random() * 20) + 1;
   operand2 = Math.floor(Math.random() * 20) + 1;
-  document.getElementById("operand1").
+  document.getElementById("operand1").innerHTML=operand1;
+  document.getElementById("operand2").innerHTML=operand2;
+  document.getElementById("results").innerHTML="";
 }
