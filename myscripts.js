dd4e0db52672e7d9c0dc0ce0d004c74392c2f917
stuy <stuy@newroads.org>
2017-01-28 13:18:23 -0500

    add test

--- myscripts.js
+++ myscripts.js
@@ -3,6 +3,8 @@ var operand1;
 var operand2;
 
 function getNumbers(){
+  //test
+  alert("test");
   operand1 = Math.floor(Math.random() * 20) + 1;
   operand2 = Math.floor(Math.random() * 20) + 1;
   document.getElementById("operand1").innerHTML=operand1;
