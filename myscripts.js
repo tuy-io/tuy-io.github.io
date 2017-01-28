aa6f3f85ea66271af4bc5dc131be0695bb383953
stuy <stuy@newroads.org>
2017-01-28 13:08:45 -0500

    add html for flashcard

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+
+var operand1;
+var operand2;
+
+function getNumbers(){
+  operand1 = Math.floor(Math.random() * 20) + 1;
+  operand2 = Math.floor(Math.random() * 20) + 1;
+  document.getElementById("operand1").
+}
