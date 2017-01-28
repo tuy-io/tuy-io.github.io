3b6778faef53997899e32d84f66449ba6585a1ab
stuy <stuy@newroads.org>
2017-01-28 13:33:23 -0500

    add test

--- myscripts.js
+++ myscripts.js
@@ -3,11 +3,21 @@ var operand1;
 var operand2;
 
 function getNumbers(){
-  //test
-  alert("test");
   operand1 = Math.floor(Math.random() * 20) + 1;
   operand2 = Math.floor(Math.random() * 20) + 1;
   document.getElementById("operand1").innerHTML=operand1;
   document.getElementById("operand2").innerHTML=operand2;
   document.getElementById("results").innerHTML="";
+}
+
+function checkAnswer(){
+  var userSum = document.getElementById("userAnswer").value;
+  var sum = operand1 + operand2;
+  
+  if(userSum == sum){
+    document.getElementById("results").innerHTML="Correct!";
+  }
+  else{
+    document.getElementById("results").innerHTML="Incorrect!";
+  }
 }
